d9cfaa853860745dde49033288bcaa73fd1b1e88
Momo Kornher <mail@moritzkornher.de>
2023-09-26 17:43:13 +0100

    MVP

---
+++ app.ts
@@ -0,0 +1,77 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
+import * as kms from "aws-cdk-lib/aws-kms";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subscriptions from "aws-cdk-lib/aws-sns-subscriptions";
+
+export class AbstractionsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /**
+     * Encrypt everything with this key
+     */
+    const key = new kms.Key(this, "Key");
+
+    /**
+     * A topic will receive data from somewhere
+     */
+    const sourceTopic = new sns.Topic(this, "Topic", {
+      masterKey: key,
+    });
+
+
+    /**
+     * Subscribe the topic to a queue, using a filter
+     * other messages will go onto the DLQ
+     */
+    const targetQueue = new sqs.Queue(this, "Queue", {
+      visibilityTimeout: cdk.Duration.seconds(300),
+      encryptionMasterKey: key,
+    });
+
+    const dlq = new sqs.Queue(this, "SubDlq", {
+      encryptionMasterKey: key,
+    });
+
+    sourceTopic.addSubscription(
+      new subscriptions.SqsSubscription(targetQueue, {
+        deadLetterQueue: dlq,
+        filterPolicy: {
+          color: sns.SubscriptionFilter.stringFilter({
+            allowlist: ["red", "orange"],
+            matchPrefixes: ["bl"],
+          }),
+        },
+      })
+    );
+
+    /**
+     * Setup a dashboard with widgets for all used resources
+     */
+    const dashboard = new cloudwatch.Dashboard(this, "Dashboard", {
+      dashboardName: "Observability",
+    });
+    dashboard.addWidgets(
+      new cloudwatch.GraphWidget({
+        title: 'Processed Messages',
+        left: [
+          sourceTopic.metricNumberOfMessagesPublished(),
+          targetQueue.metricNumberOfMessagesReceived()
+        ]
+      }),
+      new cloudwatch.GraphWidget({
+        title: 'Filtered Messages',
+        left: [
+          sourceTopic.metricNumberOfNotificationsFilteredOut(),
+          dlq.metricNumberOfMessagesReceived()
+        ]
+      })
+    );
+  }
+}
+
+const app = new cdk.App();
+new AbstractionsStack(app, "MyStack", {});
